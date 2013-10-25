0f638d43526efeec0ed2eb67c7dae1b96026cb60
shankao <shankao@gmail.com>
2013-10-25 21:27:49 +0800

    Fix for keys with more than 2 levels

--- scripts/getconfig.php
+++ scripts/getconfig.php
@@ -5,7 +5,7 @@ Returns the site's configuration
 function print_config_values ($config, $prefix = '') {
 	foreach ($config as $key => $value) {
 		if (is_array($value)) {
-			print_config_values($value, "$key.");
+			print_config_values($value, "$prefix$key.");
 		} else {
 			echo "$prefix$key=$value\n";
 		}
