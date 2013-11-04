90454ad532438f0892e0f85cab8c632fcb6fa898
shankao <shankao@gmail.com>
2013-11-04 13:12:21 +0800

    Fix PHP error reporting not showing in the logs
    The Log class must be defined prior to the initialization of the
    aftertime framework. Move the require statement just before that.

--- index.php
+++ index.php
@@ -3,10 +3,10 @@ $time_start = microtime(true);
 ini_set ('include_path', '.' . PATH_SEPARATOR . 'lib/pear/php');	// Add PEAR's folder. XXX This is needed before the includes just for their paths to be correct
 
 require_once 'include/config.php';
+require_once 'include/log.php';
 if (aftertime_init() === false) {
 	TemplateLog::render('templates/apperror.php');
 } else {
-	require_once 'include/log.php';
 	require_once 'include/app.php';
 
 	$app = appFactory::getApp($_REQUEST);
