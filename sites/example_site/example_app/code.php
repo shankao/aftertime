eadaf2047ce9e3aa5de3103735d8fb64eb9d08d3
shankao <shankao@gmail.com>
2013-12-13 19:28:42 +0800

    Make the example function explicitely public
    ...and stupid changes in the comments

--- sites/example_site/example_app/code.php
+++ sites/example_site/example_app/code.php
@@ -1,13 +1,12 @@
 <?php
 /* 
 Main class for our example app.
-The name of the class must be the app's ID
+The name of the class must match the app ID
 */
 class example_app extends app {
 	// Custom validator
-	// It can return multiple error codes in an array
-	// Return false for "no error found"
-	function my_validator($param, $request) {
+	// It can return multiple error codes in an array or false for "no error found"
+	public function my_validator($param, $request) {
 		if ($param == 'test' && $request['int'] == '5') {
 			return false;
 		} else {
