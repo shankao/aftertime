7dde6d09a1b1f09ae7a2e945d77f44c87a752e25
shankao <shankao@gmail.com>
2014-10-14 18:09:46 +0800

    Use class' method instead
    Note that this script is outdated and cannot work with the current
    code to the old aftertime_init() function. Fix it is trivial though

--- scripts/getconfig.php
+++ scripts/getconfig.php
@@ -1,28 +1,12 @@
 <?php /*
 Returns the site's configuration
 */
-
-function print_config_values ($config, $prefix = '') {
-	if (empty($config)) {
-		$prefix = substr($prefix, 0, -1);
-		echo "$prefix=\n";
-	} else {
-		foreach ($config as $key => $value) {
-			if (is_array($value)) {
-				print_config_values($value, "$prefix$key.");
-			} else {
-				echo "$prefix$key=$value\n";
-			}
-		}
-	}
-}
-
 require_once __DIR__.'/../include/config.php';
 if (aftertime_init(false, '../..') === false) {
 	echo Config::init_log();
 	exit (-1);
 }
 
-print_config_values(Config::get());
+Config::print_values();
 exit (0);
 ?>
