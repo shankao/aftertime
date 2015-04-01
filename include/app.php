33b89c606b4d05ed87f550d831a15696817f51ef
shankao <shankao@gmail.com>
2015-04-01 11:41:32 +0800

    Separate users code into a different class

--- include/app.php
+++ include/app.php
@@ -3,6 +3,7 @@ require_once __DIR__.'/log.php';
 require_once __DIR__.'/config.php';
 require_once __DIR__.'/db.php';
 require_once __DIR__.'/helpers.php';
+require_once __DIR__.'/user.php';
 
 // Returns an App object chosen from the config and request params
 final class appFactory {
@@ -87,121 +88,9 @@ abstract class app {
 
 	public $errors = array();	// Errors from the previous app
 	public $params;			// Params accepted by the app
-	public $user;			// User information. Not every site has it
+	public $user;			// Instance of Aftertime\User. Not every site has it
 	public $template;		// Rendering page
 	public $db;			// Database connection
-
-	/*
-	Functions for user authentication. 
-	It requires a PDO connection up and running with a table called 'users'
-	*/
-	private function do_login ($email, $password, $save_cookie=false, $password_is_encrypted=false) {
-
-		log_entry ("Login user $email");
-
-		$this->clean_login_cookies();
-
-		// Get user for the given email
-		$db_get_mail = $this->db->prepare('SELECT * FROM users WHERE email = :email');
-		$db_get_mail->execute(array('email' => $email));
-		$user = $db_get_mail->fetch();
-
-		if (!$user) {
-			$this->error_add('NO_USER_FOUND');
-			log_entry ("User not found for email '$email'");
-			return false;
-		} else if (!$this->check_password($password, $user['password'], $password_is_encrypted)) {
-			$this->error_add('WRONG_PASSWD');
-			log_entry ("Wrong password for email '$email'");
-			return false;
-		}
-
-		$_SESSION['user'] = $user;
-		if ($save_cookie) {	// Save for 10 days
-			setcookie('us', $email, time()+864000, '/', null, false, true);
-			setcookie('pw', $user['password'], time()+864000, '/', null, false, true);
-		}
-		log_entry ("User logged in successfully: $email");
-		return true;
-	}
-
-	private function clean_login_cookies() {
-		setcookie('us', '', time()-3600, '/', null, false, true);
-		setcookie('pw', '', time()-3600, '/', null, false, true);
-	}
-
-	// Basic auth. code taken from http://www.php.net/manual/en/features.http-auth.php
-	private function check_http_auth() {
-		$c = Config::get('apps.'.get_class($this));
-		if (isset($c['user']) && isset($c['passwd'])) {
-			// TODO Check better auth. ways instead of "Basic". Maybe autogenerating .htaccess and .htpasswd files
-			// See http://pear.php.net/manual/en/package.filesystem.file-htaccess.intro.php
-log_entry(print_r($_SERVER, true), 20000);
-
-			$user = false;
-			if (isset($_SERVER['PHP_AUTH_USER'])) {
-				$user = $_SERVER['PHP_AUTH_USER'];
-				$passwd = $_SERVER['PHP_AUTH_PW'];
-			} else {
-				// fixme This does not work when PHP is working as CGI/FastCGI
-			}
-
-			if ($user === $c['user'] && $passwd === $c['passwd']) {
-				log_entry("HTTP auth OK");
-			} else {
-				header('WWW-Authenticate: Basic realm="Login please"');
-				header('HTTP/1.0 401 Unauthorized');
-				log_entry("HTTP auth failed");
-				return false;
-			}
-		}
-		return true;
-	}
-
-//------------------------ protected
-
-	// This is the default. Note that apps. are free to override it. XXX Convert to interface?
-	// TODO Check this (PHP 5.5+): http://www.php.net/manual/en/book.password.php
-	protected function encrypt_password ($password) {
-		if (empty($password)) return null;
-
-		if (CRYPT_BLOWFISH) {
-			$type = 'blowfish';
-			$code = '2y'; 	// Blowfish code from php 5.3.7
-			$cost = '05';	// range [04,31]
-			for ($i=0, $salt=''; $i<22; $i++)		// [./0-9A-Za-z]{22} more chars are ignored
-				$salt .= rand(0, 9);
-
-			$combined_salt = "\$$code\$$cost\$$salt";
-			$result = crypt($password, $combined_salt);
-		} else {	
-			// TODO To be removed. Needs an upgrade path to safer algorithms:
-			// 1st, not to generate new passwords like this. Then, use your own legacy check_password function
-			$type = 'sha1';
-			$result = sha1($password);
-		}
-
-		log_entry ("Encrypted password $result using $type");
-		return $result;
-	}
-
-	protected function check_password ($input, $encrypted, $input_is_encrypted=false) {
-
-		log_entry("check_password (XXX, $encrypted, $input_is_encrypted)");	
-		if (empty($input) || empty($encrypted)) return false;
-
-		if ($input_is_encrypted) {
-			$result = ($input === $encrypted)? true : false;
-		} else {
-			if (CRYPT_BLOWFISH) {
-				$result = (crypt($input, $encrypted) == $encrypted)? true : false;
-			} else {	// TODO to go
-				$result = (sha1($input) == $encrypted)? true : false;
-			}
-		}
-
-                return $result;
-	}
 
 	protected function db_error() {
 		$error = db_error();	// That's the function in include/helpers.php
@@ -249,9 +138,8 @@ log_entry(print_r($_SERVER, true), 20000);
 
 		$this->db = init_db();
 		if ($this->db) {
-			if (!$this->is_user_logged() && isset($_COOKIE['us']) && isset($_COOKIE['pw'])) {	// Autologin from cookies
-				$this->do_login($_COOKIE['us'], $_COOKIE['pw'], true, true);	// TODO check login result
-			}
+			$this->user = new User($this->db);
+			$this->user->login();
 		} else {
 			log_entry('Error initializing DB');
 		}
@@ -276,27 +164,6 @@ log_entry(print_r($_SERVER, true), 20000);
 		return $result;
 	}
 	
-	public function login ($email, $password, $save_cookie=false) {
-		return $this->do_login ($email, $password, $save_cookie);
-	}
-
-	public function logout () {
-		if ($this->is_user_logged()) {
-			log_entry ("Logging out user {$this->user['email']}");
-			unset($_SESSION['user']);
-			$this->clean_login_cookies();
-		}
-	}
-
-	public function is_user_logged() {
-		if (isset($_SESSION['user'])) {
-			$this->user = $_SESSION['user'];
-			return true;
-		} else {
-			return false;
-		}
-	}
-
 	private function init_template() {
 		$page_config = Config::get("apps.{$this->params['app']}.pages.{$this->params['page']}");
 		if (!isset($page_config['template'])) {
@@ -324,8 +191,8 @@ log_entry(print_r($_SERVER, true), 20000);
 		if (isset($this->errors)) {
 			$vars['errors'] = $this->errors;
 		}
-		if ($this->is_user_logged()) {
-			$vars['user'] = $this->user;
+		if ($this->user->is_user_logged()) {
+			$vars['user'] = $this->user->toArray();
 		}
 		$vars['config'] = $config;
 		$vars['debug'] = $this->debug();
---
+++ include/user.php
@@ -0,0 +1,167 @@
+<?php
+/*
+   User management 
+   It requires a PDO connection up and running with a table called 'users'
+ */
+class User extends PDOClass {
+	const OK = 'OK';
+	const NO_EMAIL_PASSWD = 'NO_EMAIL_PASSWD';
+	const NO_USER_FOUND = 'NO_USER_FOUND';
+	const WRONG_PASSWD = 'WRONG_PASSWD';
+
+	const COOKIE_USER = '_us';
+	const COOKIE_PASSWD = '_pw';
+	const COOKIE_TIME = 864000;	// 10 days
+
+	const SESSION_VARNAME = '_user';
+
+	protected $_table = 'users';
+	protected $_fields = ['user_id', 'email', 'password'];
+	protected $_key = 'user_id';
+
+	private function get_user_from_session() {
+		$sdata = unserialize($_SESSION[User::SESSION_VARNAME]);
+		foreach ($sdata as $name => $value) {
+			$this->$name = $value;
+		}
+	}
+
+	private function put_user_in_session() {
+		$_SESSION[User::SESSION_VARNAME] = serialize($this->toArray());
+	}
+
+	private function clean_user_session() {
+		unset($_SESSION[User::SESSION_VARNAME]);
+	}
+
+	private function is_user_in_session() {
+		return isset($_SESSION[User::SESSION_VARNAME]);
+	}
+
+	private function set_cookies($email, $password) {
+		// Keep for 10 days
+		setcookie(User::COOKIE_USER, $email, time()+User::COOKIE_TIME, '/', null, false, true);
+		setcookie(User::COOKIE_PASSWD, $password, time()+User::COOKIE_TIME, '/', null, false, true);
+	}
+
+	private function clean_cookies() {
+		setcookie(User::COOKIE_USER, '', time()-3600, '/', null, false, true);
+		setcookie(User::COOKIE_PASSWD, '', time()-3600, '/', null, false, true);
+	}
+
+	private function get_cookies() {
+		if (isset($_COOKIE[User::COOKIE_USER]) && isset($_COOKIE[User::COOKIE_PASSWD])) {
+			$result['email'] = $_COOKIE[User::COOKIE_USER];
+			$result['password'] = $_COOKIE[User::COOKIE_PASSWD];
+			return $result;
+		} else {
+			return false;
+		}
+	}
+
+	// Basic auth. code taken from http://www.php.net/manual/en/features.http-auth.php
+	private function check_http_auth() {
+		$c = Config::get('apps.'.get_class($this));
+		if (isset($c['user']) && isset($c['passwd'])) {
+			// TODO Check better auth. ways instead of "Basic". Maybe autogenerating .htaccess and .htpasswd files
+			// See http://pear.php.net/manual/en/package.filesystem.file-htaccess.intro.php
+
+			$user = false;
+			if (isset($_SERVER['PHP_AUTH_USER'])) {
+				$user = $_SERVER['PHP_AUTH_USER'];
+				$passwd = $_SERVER['PHP_AUTH_PW'];
+			} else {
+				// fixme This does not work when PHP is working as CGI/FastCGI
+			}
+
+			if ($user === $c['user'] && $passwd === $c['passwd']) {
+				// HTTP auth OK
+			} else {
+				header('WWW-Authenticate: Basic realm="Login please"');
+				header('HTTP/1.0 401 Unauthorized');
+				return false;
+			}
+		}
+		return true;
+	}
+
+	// This is the default. Note that apps. are free to override it. XXX Convert to interface?
+	// TODO Check this (PHP 5.5+): http://www.php.net/manual/en/book.password.php
+	public function encrypt_password ($password) {
+		if (empty($password)) return null;
+
+		$type = 'blowfish';
+		$code = '2y'; 	// Blowfish code from php 5.3.7
+		$cost = '05';	// range [04,31]
+		for ($i=0, $salt=''; $i<22; $i++)		// [./0-9A-Za-z]{22} more chars are ignored
+			$salt .= rand(0, 9);
+
+		$combined_salt = "\$$code\$$cost\$$salt";
+		$result = crypt($password, $combined_salt);
+		return $result;
+	}
+
+	public function check_password ($input, $encrypted, $input_is_encrypted=false) {
+		if (empty($input) || empty($encrypted)) return false;
+
+		if ($input_is_encrypted) {
+			$result = ($input === $encrypted)? true : false;
+		} else {
+			$result = (crypt($input, $encrypted) == $encrypted)? true : false;
+		}
+		return $result;
+	}
+
+	public function login ($email = '', $password = '', $set_cookies=false) {
+		if ($this->is_user_logged()) {
+			$this->get_user_from_session();
+		} else {
+			$password_is_encrypted = false;
+			if (empty($email) || empty($password)) {	// Try cookies auth
+				$cookies = $this->get_cookies();
+				if ($cookies === false) { 
+					return User::NO_EMAIL_PASSWD;	// No more methods to try
+				}
+				$email = $cookies['email'];
+				$password = $cookies['password'];
+				$password_is_encrypted = true;
+				$set_cookies = true;	// Keep them
+			}
+
+			$this->clean_cookies();
+			$user = $this->find('email', $email);
+			if (!$user) {
+				return User::NO_USER_FOUND;
+			}
+			$user = $user[0];
+
+			foreach ($this->_fields as $var) {
+				if (isset($user[$var])) {
+					$this->$var = $user[$var];
+				}
+			}
+
+			if (!$this->check_password($password, $user['password'], $password_is_encrypted)) {
+				return User::WRONG_PASSWD;
+			}
+
+			$this->put_user_in_session();
+			if ($set_cookies) {	
+				$this->set_cookies($user['email'], $user['password']);	// Note that it's the encrypted passwd
+			}
+		}
+		return User::OK;
+	}
+
+	public function logout () {
+		if ($this->is_user_logged()) {
+			$this->clean_user_session();
+			$this->clean_cookies();
+		}
+	}
+
+	public function is_user_logged() {
+		return $this->is_user_in_session();
+	}
+}
+?>
