918e88aed6fbda8110337150f94d186ed817bc72
shankao <shankao@gmail.com>
2015-06-24 15:47:28 +0800

    Add missing require dependency

--- include/log.php
+++ include/log.php
@@ -2,6 +2,7 @@
 namespace Aftertime;
 
 require_once __DIR__.'/helpers.php';
+require_once __DIR__.'/config.php';
 
 function log_entry ($text, $sizelimit = 2000) {
 	return Log::log_entry ($text, $sizelimit);
