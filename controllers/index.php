2046920006922529960a58c2b5b9a486e6203c8c
shankao <shankao@gmail.com>
2015-10-16 10:34:17 +0800

    Update example controller

--- controllers/index.php
+++ controllers/index.php
@@ -1,7 +1,5 @@
 <?php
 require_once 'aftertime/include/aftertime.php';
-$aftertime = new Aftertime;
-$aftertime->framework_folder = 'aftertime';
-$aftertime->config = 'socialads/config';
+$aftertime = new Aftertime('socialads/config');
 $aftertime->run_app();
 ?>
