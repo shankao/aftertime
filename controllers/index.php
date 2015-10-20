ef525cd9ccd6c2461e0fed21eb99be9a8d2da4f5
shankao <shankao@gmail.com>
2015-10-21 01:07:12 +0800

    Update the example site a bit. Untested

--- controllers/index.php
+++
@@ -1,5 +0,0 @@
-<?php
-require_once 'aftertime/include/aftertime.php';
-$aftertime = new Aftertime('socialads/config');
-$aftertime->run_app();
-?>
---
+++ example_site/index.php
@@ -0,0 +1,5 @@
+<?php
+require_once 'aftertime/vendor/autoload.php';
+$aftertime = new Aftertime('config');
+$aftertime->run_app();
+?>
