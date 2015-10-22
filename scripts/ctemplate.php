fc99167a78766c883aa5c01137a6d25b7870ed02
shankao <shankao@gmail.com>
2015-10-22 12:09:33 +0800

    Fix script

--- scripts/ctemplate.php
+++ scripts/ctemplate.php
@@ -1,17 +1,16 @@
 <?php
-require_once __DIR__.'/../vendor/autoload.php';
-
-$opts = getopt('t:');
+$opts = getopt('t:c:');
 if (!isset($opts['t'])) {
 	echo "Please, specify the template to output with the -t option\n";
 	exit(1);
 }
-
-if (!aftertime_init(false, '../..')) {
-        echo Config::init_log();
-        exit (1);
+if (!isset($opts['c'])) {
+	echo "Please, specify the config folder with the -c option\n";
+	exit(1);
 }
-$config = Config::get();
+
+require_once __DIR__.'/../vendor/autoload.php';
+$aftertime = new Aftertime\Aftertime($opts['c']);
 $result = Aftertime\template_render($opts['t'], null, false);
 exit($result? 0 : 1);
 ?>
