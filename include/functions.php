32806dadc9f7d1afa036960f1f8af4347e863068
shankao <shankao@gmail.com>
2015-10-23 18:50:44 +0800

    Remove: it gives more problems than it solves

--- include/functions.php
+++ include/functions.php
@@ -49,31 +49,4 @@ function template_render($filename, array $vars = null, $logging = true) {
 	$t = new Template($filename, $vars, $logging);
 	return $t->render();
 }
-
-// Checks write permissions if the file exists, or creates it of it doesn't
-// Same thing for folders
-function create_file($filename, $is_folder = false, $mode = false) {
-	$result = true;
-	if (is_writeable($filename) === false) {
-		if (file_exists($filename)) {
-			$result = false;	// permissions problem?
-		}
-
-		$oldumask = umask(0);
-		if ($is_folder) { 
-			$result = mkdir($filename, $mode, true);
-		} else if (!@touch($filename)) {
-			$result = false;
-		} else {	// No folder, touch() OK
-			if ($result && $mode) {
-				$result = chmod($filename, $mode);
-			}
-		}
-		if ($result === true) {
-			log_entry ("$filename created");
-		}
-		umask($oldumask);
-	}
-	return $result;
-}
 ?>
