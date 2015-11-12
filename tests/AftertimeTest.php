5ef6977f27a2dbc3ac58d64d322bfc9ba0fe0a8c
shankao <shankao@gmail.com>
2015-11-12 14:28:50 +0800

    Test for logs init exception

--- tests/AftertimeTest.php
+++ tests/AftertimeTest.php
@@ -6,8 +6,7 @@ use Aftertime\Aftertime;
 class AftertimeTest extends PHPUnit_Framework_TestCase {
 	/**
 	* @expectedException Aftertime\AftertimeException
-	* @expectedExceptionMessage Can't initialize config
-	* @expectedExceptionCode 0
+	* @expectedExceptionCode Aftertime\AftertimeException::E_CONFIG_INIT
 	*/
 	public function test_bad_config() {
 		$this->expectOutputRegex(<<<EOT
@@ -19,5 +18,15 @@ EOT
 		);
 		$aftertime = new Aftertime('badconfiglocation');
 	}
+
+	/**
+	* @expectedException Aftertime\AftertimeException
+	* @expectedExceptionCode Aftertime\AftertimeException::E_LOG_INIT
+	*/
+	public function test_log_init_error() {
+		$this->expectOutputString("No 'logs' key present in the config\n");
+		$aftertime = new Aftertime(__DIR__.'/../config');
+	}
+
 }
 ?>
